01bb159b4ec45819641a51984a130e3da5be1eb7
bcm22 <135455152+bcm22@users.noreply.github.com>
2023-07-27 18:24:28 -0500

    Started JS file

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var APIKey = '73864aa08bb0fe712a58cb3ffefbb391';
+var search = $('#search-button');
+var city = "";
+var cityHistory = $('#city-history');
+var todayJS = dayjs().format('M/D/YYY');
+
+    
